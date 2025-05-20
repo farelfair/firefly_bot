e4453d5ceeaa7322e2922d53a217126008534460
Mhmd Farel <107840579+farelfair@users.noreply.github.com>
2025-05-20 22:52:34 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# Nama file
+# ===== file path example /home/farelfair/file.zip =====
 FILE=""
 
-# Cek file
+# ===== check file =====
 if [ ! -f "$FILE" ]; then
   echo "[✗] File '$FILE' tidak ditemukan!"
   exit 1
 fi
 
-# Atur zona waktu ke Asia/Jakarta (WIB)
+# ===== set time zone =====
 export TZ="Asia/Jakarta"
 
-# Metadata
+# ===== metadata =====
 FILE_SIZE=$(du -h "$FILE" | cut -f1)
 UPLOAD_DATE=$(date "+%Y-%m-%d %H:%M:%S %Z")  # Contoh: 2025-05-20 23:40:00 WIB
 MD5_SUM=$(md5sum "$FILE" | cut -d ' ' -f1)
 SHA1_SUM=$(sha1sum "$FILE" | cut -d ' ' -f1)
 
-# Minta link download Google Drive
-read -p "Masukkan Link Google Drive: " DOWNLOAD_URL
+# ===== Paste link =====
+read -p "enter the file download link: " DOWNLOAD_URL
 
-# Buat file JSON payload Telegram
+# ===== created file JSON payload Telegram =====
 cat <<EOF > telegram_payload.json
 {
   "text": "*File Information:*\n*File Name:* \`$FILE\`\n*File Size:* \`$FILE_SIZE\`\n*Upload Date:* \`$UPLOAD_DATE\`\n*MD5 Sum:* \`$MD5_SUM\`\n*SHA1 Sum:* \`$SHA1_SUM\`",
@@ -39,4 +39,4 @@ cat <<EOF > telegram_payload.json
 }
 EOF
 
-echo "[✓] Payload Telegram siap di 'telegram_payload.json' (Zona waktu: $TZ)"
+echo " payload successfully created 'telegram_payload.json' (Timezone: $TZ)"
