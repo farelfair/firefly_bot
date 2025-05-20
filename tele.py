297878c74168cfa61287df0b01611df90cc14759
Mhmd Farel <107840579+farelfair@users.noreply.github.com>
2025-05-20 22:53:59 +0800

    Update tele.py

--- tele.py
+++ tele.py
@@ -3,8 +3,8 @@ from telegram import Bot, InlineKeyboardMarkup, InlineKeyboardButton
 import json
 
 async def main():
-    bot = Bot("")
-    chat_id = ""
+    bot = Bot("") # paste token bot in here
+    chat_id = "" # paste chat id
 
     with open("telegram_payload.json") as f:
         data = json.load(f)
@@ -14,5 +14,5 @@ async def main():
 
     await bot.send_message(chat_id=chat_id, text=text, parse_mode=data["parse_mode"], reply_markup=keyboard)
 
-# Jalankan event loop
+# ===== run event loop =====
 asyncio.run(main())
