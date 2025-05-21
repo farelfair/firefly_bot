ca8574d4a3b256882f5627a2b309a55c35e0d981
Mhmd Farel <107840579+farelfair@users.noreply.github.com>
2025-05-21 12:32:07 +0800

    fixing path

--- generate.sh
+++ generate.sh
@@ -2,6 +2,7 @@
 
 # ===== file path example /home/farelfair/file.zip =====
 FILE=""
+FILE_NAME=$(basename "$FILE")
 
 # ===== check file =====
 if [ ! -f "$FILE" ]; then
@@ -26,7 +27,7 @@ read -p "enter the file download link: " DOWNLOAD_URL
 # ===== created file JSON payload Telegram =====
 cat <<EOF > telegram_payload.json
 {
-  "text": "*File Information:*\n*File Name:* \`$FILE\`\n*File Size:* *$FILE_SIZE*\n*Upload Date:* \`$UPLOAD_DATE\`\n*Maintainer:* $MAINTAINER\n*Device:* *$DEVICE*\n*MD5 Sum:* \`$MD5_SUM\`\n*SHA1 Sum:* \`$SHA1_SUM\`",
+  "text": "*File Information:*\n*File Name:* \`$FILE_NAME\`\n*File Size:* *$FILE_SIZE*\n*Upload Date:* \`$UPLOAD_DATE\`\n*Maintainer:* $MAINTAINER\n*Device:* *$DEVICE*\n*MD5 Sum:* \`$MD5_SUM\`\n*SHA1 Sum:* \`$SHA1_SUM\`",
   "parse_mode": "Markdown",
   "reply_markup": {
     "inline_keyboard": [
