798531a4bef01bb64aefbf01ac35e1e4e231f400
Mhmd Farel <107840579+farelfair@users.noreply.github.com>
2025-05-21 09:01:11 +0800

    add maintainer & device info remove backtick like `

--- generate.sh
+++ generate.sh
@@ -15,6 +15,8 @@ export TZ="Asia/Jakarta"
 # ===== metadata =====
 FILE_SIZE=$(du -h "$FILE" | cut -f1)
 UPLOAD_DATE=$(date "+%Y-%m-%d %H:%M:%S %Z")  # Contoh: 2025-05-20 23:40:00 WIB
+MAINTAINER="@renvelations"
+DEVICE="poco M3 & Redmi 9T / Redmi 9 Power"
 MD5_SUM=$(md5sum "$FILE" | cut -d ' ' -f1)
 SHA1_SUM=$(sha1sum "$FILE" | cut -d ' ' -f1)
 
@@ -24,7 +26,7 @@ read -p "enter the file download link: " DOWNLOAD_URL
 # ===== created file JSON payload Telegram =====
 cat <<EOF > telegram_payload.json
 {
-  "text": "*File Information:*\n*File Name:* \`$FILE\`\n*File Size:* \`$FILE_SIZE\`\n*Upload Date:* \`$UPLOAD_DATE\`\n*MD5 Sum:* \`$MD5_SUM\`\n*SHA1 Sum:* \`$SHA1_SUM\`",
+  "text": "*File Information:*\n*File Name:* \`$FILE\`\n*File Size:* *$FILE_SIZE*\n*Upload Date:* \`$UPLOAD_DATE\`\n*Maintainer:* $MAINTAINER\n*Device:* *$DEVICE*\n*MD5 Sum:* \`$MD5_SUM\`\n*SHA1 Sum:* \`$SHA1_SUM\`",
   "parse_mode": "Markdown",
   "reply_markup": {
     "inline_keyboard": [
